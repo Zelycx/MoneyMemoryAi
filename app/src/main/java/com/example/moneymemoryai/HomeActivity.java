0918cb0bd4bccb730b1b87057758b1c17ddc2eea
Zelycx <ezekielmores084@gmail.com>
2026-07-29 10:41:57 +0800

    Initial commit
    Splash Integrated, Currently Learning while doing the application itself. latest update: gain button action.

---
+++ app/src/main/java/com/example/moneymemoryai/HomeActivity.java
@@ -0,0 +1,40 @@
+package com.example.moneymemoryai;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class HomeActivity extends AppCompatActivity {
+
+    Button btnGain;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home);
+
+        btnGain = findViewById(R.id.btnGain); // the initialization of gain button
+        // bottom are the onclick listeners for the button
+        btnGain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnGain.setText("Clicked!");
+            }
+        });
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
