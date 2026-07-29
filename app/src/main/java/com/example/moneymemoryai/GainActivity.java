2a075dca23762d1e9fdba959196f38636b8e86ad
Zelycx <ezekielmores084@gmail.com>
2026-07-29 20:24:17 +0800

    Created the whole Gain Activity or Gain/Income Feature
    did tons of things, learn tons of things, and understand tons of things such as scrolling, date picker, linear layout, match_parent, 0dp, and more.

--- app/src/main/java/com/example/moneymemoryai/GainActivity.java
+++ app/src/main/java/com/example/moneymemoryai/GainActivity.java
@@ -1,6 +1,12 @@
 package com.example.moneymemoryai;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Button;
+import android.app.DatePickerDialog;
+import android.widget.Toast;
+import java.util.Calendar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +16,41 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class GainActivity extends AppCompatActivity {
 
+    Calendar calendar;
+    DatePickerDialog datePickerDialog;
+    ImageButton btnBack;
+    Button btnSelectDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_gain);
+
+
+        btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }});
+
+        btnSelectDate = findViewById(R.id.btnSelectDate);
+        calendar = Calendar.getInstance();
+
+        btnSelectDate.setOnClickListener(v -> {
+            datePickerDialog = new DatePickerDialog(GainActivity.this,
+                    (view, year, month, dayOfMonth) -> {
+                        btnSelectDate.setText(dayOfMonth + "/" + (month + 1) + "/" + year);
+                    },
+                    calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH)
+            );
+
+            datePickerDialog.show();
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
